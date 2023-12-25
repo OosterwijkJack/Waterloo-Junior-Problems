160c4a88a49c3e838cec8439792456c5695d4c70
jack <oosterwijkcools@gmail.com>
2023-12-25 13:36:40 -0400

    All tests pass for problem 1

---
+++ 2005/Problem 1/main.py
@@ -0,0 +1,74 @@
+import os
+# The Cell Sell
+"""
+The function of this program is to caclulate the best cell phone usage plan
+
+"""
+
+def main():
+    here = os.getcwd()
+    data_index = 3 # which test data fild is used
+
+    datain = open(os.path.join(here,f"testing/j1.{data_index}.in"), "r").read().splitlines() # in data
+    dataout = open(os.path.join(here,f"testing/j1.{data_index}.out"), "r").read().splitlines() # correct corresponding out data
+    out = [] # program output
+
+    day = int(datain[0])
+    even = int(datain[1])
+    week = int(datain[2])
+    # calculate plan A
+    A_cost = A_price(day,even,week)
+    # calculate plan B
+    B_cost = B_price(day,even,week)
+
+    # print or append test data
+    out.append(f"Plan A costs {A_cost}") 
+    out.append(f"Plan B costs {B_cost}")
+    #print(f"Plan A costs {A_cost}")
+    #print(f"Plan B costs {B_cost}")
+
+    if  A_cost == B_cost:
+        out.append("Plan A and B are the same price.")
+        #print("Plan A and B are the same price")
+    else:
+        out.append("Plan A is cheapest." if A_cost < B_cost else "Plan B is cheapest.")
+        #print("Plan A is cheapest" if A_cost < B_cost else "Plan B is cheapest")
+    testing(dataout, out)
+
+def testing(dataout, out):
+    # compare correct outputs and program output
+    for a,i in zip(dataout, out):
+        if a == i:
+            print(":) good")
+        else:
+            print(":( Bad")
+            print(f"correct output: {a}")
+            print(f"your output: {i}")
+
+def A_price(day, evening, weekend):
+    # 100 free minutes then 25 cents a minute
+    if day > 100:
+        day = (day-100) * 0.25
+    else:
+        day = 0
+
+    evening = evening * 0.15 # 15 cents a minute
+    weekend = weekend * 0.20 # 20 cents a minute
+
+    a = (day, evening,weekend)
+    return round(sum(a),2) # sum the values and round to second decimal place
+    
+def B_price(day, evening, weekend):
+    if day > 250:
+        day = (day-250) * 0.45
+    else:
+        day = 0
+
+    evening = evening * 0.35
+    weekend = weekend * 0.25
+
+    a = (day,evening,weekend)
+    return round(sum(a),2) # sum the values and round to second decimal place
+
+if __name__ == "__main__":
+    main() 
