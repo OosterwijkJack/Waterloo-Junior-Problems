b252dbe2a5eb8425ebca62e1c579295cf3e5ba90
jack <oosterwijkcools@gmail.com>
2024-01-06 22:25:26 -0400

    Setup problem 2.

---
+++ 2006/Problem 2/dice.py
@@ -0,0 +1,17 @@
+"""
+Name: Roll the Dice
+
+This program outputs the the total amount of ways the number 10 can be rolled
+on 2 dice given the amount of sides the dice gave assuming the sides range from
+1-number of sides
+
+"""
+
+def main():
+    pass
+
+def begin(*data):
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ 2006/Problem 2/test.py
@@ -0,0 +1,37 @@
+import dice
+from pathlib import Path
+import os
+
+
+def main():  
+    year = 2006
+    problem = 1
+    files = len(os.listdir(Path.joinpath(Path.cwd(), f"{year}/Problem {problem}/test_data/j{problem}"))) // 2
+    
+    for i in range(files):
+        index = i+1
+
+        datain = open(Path.joinpath(Path.cwd(), f"{year}/Problem {problem}/test_data/j{problem}/j{problem}.{index}.in")).read().splitlines()
+        dataout = open(Path.joinpath(Path.cwd(), f"{year}/Problem {problem}/test_data/j{problem}/j{problem}.{index}.out")).read().splitlines()
+        out = dice.begin(*datain)
+
+        if testing(dataout, out):
+            print("\n".join(out))
+
+
+def testing(dataout, out):
+    # compare correct outputs and program output
+    good = None
+    for a,i in zip(dataout, out):
+        if a == i:
+            print(":) good")
+            good = True
+        else:
+            print(":( Bad")
+            print(f"correct output: {a}")
+            print(f"your output: {i}")
+            good = False
+    return good
+
+if __name__ == "__main__":
+    main()
