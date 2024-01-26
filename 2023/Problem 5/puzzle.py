46043cfc650d8ddb56f65ebfd684bc7d12f3fafb
Jack Oosterwijk <oosterwijkcools@gmail.com>
2024-01-26 10:58:21 -0400

    On track. Needs nebugging.

--- 2023/Problem 5/puzzle.py
+++ 2023/Problem 5/puzzle.py
@@ -28,8 +28,9 @@ example of perhaps occuring in a word hunt twice
 """
 class WordMap:
 
-    def __init__(self, a,b) -> None:
-        self.data = (a,b)
+    def __init__(self, data, word) -> None:
+        self.data = data
+        self.word = word
 
     def __str__(self) -> str:
         out = ""
@@ -40,8 +41,10 @@ class WordMap:
             out += "\n"
         return out
     
+    # straight
     def get_U(self, pos):
         try:
+            not_negative(pos[0]-1)
             return self.data[pos[0]-1][pos[1]]
         except IndexError:
             return 0
@@ -52,19 +55,59 @@ class WordMap:
             return 0
     def get_L(self, pos):
         try:
+            not_negative(pos[1]-1)
             return self.data[pos[0]][pos[1]-1]
         except IndexError:
             return 0
     def get_R(self, pos):
         try:
+            return self.data[pos[0]][pos[1]+1]
+        except IndexError:
+            return 0
+    # diagonal   
+    def get_RU(self, pos):
+        try:
+            not_negative(pos[0]-1)
+            return self.data[pos[0]-1][pos[1] + 1]
+        except IndexError:
+            return 0
+    def get_RD(self, pos):
+        try:
+            return self.data[pos[0]+1][pos[1]+1]
+        except IndexError:
+            return 0
+        
+    def get_LU(self, pos):
+        try:
+            not_negative(pos[0]-1)
+            not_negative(pos[1]-1)
+            return self.data[pos[0]-1][pos[1]-1]
+        except IndexError:
+            return 0
+    def get_LD(self, pos):
+        try:
+            not_negative(pos[0]-1)
             return self.data[pos[0]-1][pos[1]+1]
         except IndexError:
             return 0
+            
+    def get_all_diretions(self, pos):
+        
+        return [
+            (self.get_L(pos),"L"),
+            (self.get_R(pos),"R"),
+            (self.get_U(pos),"U"),
+            (self.get_D(pos),"D"),
+            (self.get_LU(pos),"LU"),
+            (self.get_LD(pos),"LD"),
+            (self.get_RU(pos),"RU"),
+            (self.get_RD(pos),"RD")
+        ]
 
 class Pos:
     def __init__(self, pos) -> None:
         self.pos = pos
-    
+    #straight
     def mov_R(self):
         self.pos = (self.pos[0], self.pos[1]+1)
     def mov_L(self):
@@ -73,29 +116,122 @@ class Pos:
         self.pos = (self.pos[0]-1, self.pos[1])
     def mov_D(self):
         self.pos = (self.pos[0]+1, self.pos[1])
+
+    # diagonal
+    def mov_RU(self):
+        self.pos =  (self.pos[0]-1, self.pos[1] + 1)
+    def mov_RD(self):
+        self.pos =  (self.pos[0]+1, self.pos[1] + 1) 
+    def mov_LU(self):
+        self.pos =  (self.pos[0]-1, self.pos[1] - 1)
+    def mov_LD(self):
+        self.pos =  (self.pos[0]+1, self.pos[1] - 1)
+
     def reset_L(self):
         self.pos = (self.pos[0], 0)   
 
+    def get(self):
+        return self.pos
+
 def main():
-    word = str(input())
-    dimensions = (int(input()), int(input()))
-    wrd_map = WordMap([input().replace(" ", "") for _ in range(dimensions[0])])
+    word = "MENU"
+    dimensions = (5, 7)
+    wrd_map = """F T R U B L K
+P M N A X C U
+A E R C N E O
+M N E U A R M
+M U N E M N S""".split("\n")
+    wrd_map = WordMap([x.replace(" ", "") for x in wrd_map], word)
 
     print()
     print(wrd_map)
 
+    print(iterate_word_map(wrd_map))
+
 def iterate_word_map(wrd_map):
-    pos = Pos(0,0)
+    pos = Pos((0,0))
+    instances = 0
 
-    for i in wrd_map:
+    for i in wrd_map.data:
         for a in i:
-
-
+            valid_directions = get_valid_direction(wrd_map, pos)
+            #print(valid_directions)
+            instances += [search_direction(wrd_map, pos, x) for x in valid_directions].count(True)
             pos.mov_R() # move right
 
         pos.mov_D() # move down
         pos.reset_L() # move to beggining of list 
+    return instances
 
+
+def search_direction(wrd_map:WordMap, pos:Pos, direction): # takes direction and contiously searches for next word
+    new_pos = Pos(pos.get())
+    all_letters = ""
+    # direction is a tuple containing the path and what direction the path is going
+    for i in range(len(wrd_map.word)):
+
+        letter = get_next_letter(wrd_map, new_pos, direction[1]) # next letter in dir
+
+        if letter:
+            all_letters += letter # all letters found adds up
+        
+        if all_letters == wrd_map.word:
+            print(new_pos.pos)
+            return True
+        
+        if letter != wrd_map.word[i]:
+            return False
+    return True
+
+# gets next letter depending on position and direction
+def get_next_letter(wrd_map:WordMap, pos:Pos,direction):
+    if direction == "R":
+        next = wrd_map.get_R(pos.get())
+        pos.mov_R()
+        return next
+    if direction == "L":
+        next = wrd_map.get_L(pos.get())
+        pos.mov_L()
+        return next
+    if direction == "U":
+        next = wrd_map.get_U(pos.get())
+        pos.mov_U()
+        return next
+    if direction == "D":
+        next = wrd_map.get_D(pos.get())
+        pos.mov_D()
+        return next
+    if direction == "RU":
+        next = wrd_map.get_RU(pos.get())
+        pos.mov_RU()
+        return next
+    if direction == "RD":
+        next = wrd_map.get_RD(pos.get())
+        pos.mov_RD()
+        return next
+    if direction == "LU":
+        next = wrd_map.get_LU(pos.get())
+        pos.mov_LU()
+        return next
+    if direction == "LD":
+        next = wrd_map.get_LD(pos.get())
+        pos.mov_R()
+        return next
+    
+# return directions which next letter is the same as first letter of word
+def get_valid_direction(wrd_map:WordMap, pos: Pos):
+    directions = wrd_map.get_all_diretions(pos.get())
+    valid = []
+
+    for i in directions:
+        if i[0] == wrd_map.word[0]:
+            valid.append(i)
+    return valid
+
+def not_negative(value):
+    if value <0:
+        raise IndexError
+        
 if __name__ == "__main__":
     main()
 
